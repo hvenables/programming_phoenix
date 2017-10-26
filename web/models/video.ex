ab938e2d98c9fe6922096c659f7ff6d6ebc86bd7
Harry Venables <venablesh@yahoo.co.uk>
2017-10-26 19:19:41 +0100

    Add slug for videos for vanity urls

--- web/models/video.ex
+++ web/models/video.ex
@@ -5,6 +5,7 @@ defmodule Rumbl.Video do
     field :url, :string
     field :title, :string
     field :description, :string
+    field :slug, :string
     belongs_to :user, Rumbl.User
     belongs_to :category, Rumbl.Category
 
@@ -14,10 +15,31 @@ defmodule Rumbl.Video do
   @doc """
   Builds a changeset based on the `struct` and `params`.
   """
-  def changeset(struct, params \\ %{}) do
-    struct
+  def changeset(model, params \\ %{}) do
+    model
     |> cast(params, [:url, :title, :description, :category_id])
     |> validate_required([:url, :title, :description])
+    |> slugify_title()
     |> assoc_constraint(:category)
+  end
+
+  defp slugify_title(changeset) do
+    if title = get_change(changeset, :title) do
+      put_change(changeset, :slug, slugify(title))
+    else
+      changeset
+    end
+  end
+
+  defp slugify(title) do
+    title
+    |> String.downcase()
+    |> String.replace(~r/[^\w-]+/u, "-")
+  end
+end
+
+defimpl Phoenix.Param, for: Rumbl.Video do
+  def to_param(%{slug: slug, id: id}) do
+    "#{id}-#{slug}"
   end
 end
